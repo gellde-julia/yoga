718bca5c535267b2c0d0a7ac42f051a6a13fe610
Gellde <gelldesmodus@gmail.com>
2022-07-15 18:25:11 +0300

    tabs script

---
+++ yoga/js/script.js
@@ -0,0 +1,42 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    //функция, которая скрывает все табы, кроме 1-го 
+        function hideTabContent(a) {
+            for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+
+        }
+    }
+    //запуск этой функции
+    hideTabContent(1);
+
+    //функция, которая показывает скрытые ранее табы
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if(target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    }); 
+
+});
